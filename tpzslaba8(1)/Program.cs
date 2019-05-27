1224e8f1d6c9ad6a58bbd78b51f677b39e18c125
tenickajaalena <tenickajaalena@gmail.com>
2019-05-27 23:51:58 +0300

    Добавьте файлы проекта.

---
+++ tpzslaba8(1)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace tpzslaba8_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader file = new StreamReader("input.txt", Encoding.UTF8))
+            {
+                string text = file.ReadToEnd();
+
+                int count = text.Count(p => p == '(' || p == ')');
+                Console.WriteLine($"В этом выражении {count} скобок");
+            }
+            Console.ReadKey();
+        }
+    }
+}
